4ffd9a7362ae45c8e688323732e79a9b13025ae7
Luissebas06 <luissebastiansanchez15@gmail.com>
2023-03-17 18:41:14 -0600

    Hola

--- Laboratorio2LuisSanchez/Program.cs
+++ Laboratorio2LuisSanchez/Program.cs
@@ -6,7 +6,7 @@ namespace Laboratorio2LuisSanchez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola");
         }
     }
 }
