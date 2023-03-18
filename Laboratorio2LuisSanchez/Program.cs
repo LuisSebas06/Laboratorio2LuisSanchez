5f981a688826deea70e8b52e0a8d10d48d325541
Luissebas06 <luissebastiansanchez15@gmail.com>
2023-03-17 18:32:59 -0600

    Agregar archivos de proyecto.

---
+++ Laboratorio2LuisSanchez/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboratorio2LuisSanchez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
